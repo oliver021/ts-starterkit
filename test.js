65822712d7298efd8e6f54d40d5914e19d3c57c2
oliver021 <oliver021val@gmail.com>
2021-02-20 19:18:48 -0500

    create initial template

---
+++ test.js
@@ -0,0 +1,9 @@
+const { assert } = require("chai");
+const { usePipeline } = require("./lib/index");
+
+describe('The basic test to ensure that pipeline is working...', function(){
+    it('First...', () =>
+    {
+        /*your test here*/
+    });
+});
