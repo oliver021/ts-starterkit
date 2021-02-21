8820c241ddedcc4a7c4e3c0cb56158ca96e6b731
oliver021 <oliver021val@gmail.com>
2021-02-20 20:06:46 -0500

    add more sample source

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,33 @@
 *@description The sample class
 */
 export default class SampleClass{
-	public msg: string;
+	/**
+	*@property msg {string}
+	*@description simple msg
+	*/
+	protected msg: string;
 
 	constructor(_msg: string){
 		this.msg = _msg;
 	}
 
 	/**
-	*@function
+	*@method
 	*@description The sample method
 	*/
 	getMessage(){
+		if(this.msg === undefined){
+			throw new Error();
+		}
 		return this.msg;
+	}
+
+	/**
+	*@method
+	*@description The sample method to put message
+	*/
+	putMessage(_msg: string){
+		this.msg = _msg;
 	}
 }
 
@@ -25,5 +40,8 @@ export default class SampleClass{
 *@description the factory sample class
 */
 export function factoryClass(msg: string){
+	if(msg.length < 1){
+		throw "the message can be empty";
+	}
 	return new SampleClass(msg);
 }
